57a8c2d624936dcb567df4c443991f3fe495229d
Luna Goodnight <luna.lee@nextfortune.tw>
2023-07-10 05:07:23 +0800

    add jest

---
+++ src/app/index.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+test("loads and displays title", async () => {
+  expect(1).toBe(1);
+});
