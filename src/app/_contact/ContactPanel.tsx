80b128cb597a941f741a74fa26b070f6baed6d06
Luna Goodnight <luna.lee@nextfortune.tw>
2023-07-10 10:09:07 +0800

    add test render

--- src/app/_contact/ContactPanel.tsx
+++ src/app/_contact/ContactPanel.tsx
@@ -5,20 +5,26 @@ interface ContactResponse extends Response {
   statusCode?: number;
 }
 async function getData() {
-  const res: ContactResponse = await fetch(
-    "http://localhost:3000/api/contacts",
-    { next: { revalidate: 10 } },
-  );
-
-  return res.json();
+  try {
+    const res: ContactResponse = await fetch(
+      "http://localhost:3000/api/contacts",
+      { next: { revalidate: 10 } },
+    );
+    if (res.statusCode !== 200) {
+      return null;
+    }
+    return res.json();
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export const ContactPanel = async () => {
   const res = await getData();
   return (
-    <div className="flex gap-3 flex-col p-8 lg:p-20">
+    <div className="flex gap-3 flex-col p-8 lg:p-20 h-screen">
       <AddContactPanel />
-      {res.data.length ? (
+      {res ? (
         <ContactList list={res.data} />
       ) : (
         <div className="text-neutral-500 font-bold text-lg">No Contacts</div>
